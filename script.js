f35b92a07a15aec7e11e9caca5cb0cd53921a1cd
ImranGuluzada <imranguluzadeh@gmail.com>
2023-06-07 23:34:25 +0400

    script injected

--- script.js
+++ script.js
@@ -3,10 +3,46 @@ const birthMonth = document.querySelector(".month");
 const birthYear = document.querySelector(".year");
 const inputs = document.querySelectorAll("input");
 const dError = document.querySelector("#dayError");
+const mError = document.querySelector("#monthError");
+const yError = document.querySelector("#yearError");
+const calcDays = document.querySelector(".c-days");
+const calcMonth = document.querySelector(".c-months");
+const calcYear = document.querySelector(".c-years");
+const today = new Date();
+console.log(today);
+let age;
+let month;
+let day;
 
 const submit = document.getElementById("submit");
 
+// Validations
 birthDay.addEventListener("input", function (e) {
+  if (e.target.value <= 0 || e.target.value > 31) {
+    dError.style.display = "block";
+  } else {
+    dError.style.display = "none";
+  }
+  day = today.getDay() - e.target.value;
+  calcDays.innerHTML = day;
+});
 
-    console.log(e.target.value);
+birthMonth.addEventListener("input", function (e) {
+  if (e.target.value < 1 || e.target.value > 12) {
+    mError.style.display = "block";
+  } else {
+    mError.style.display = "none";
+  }
+  month = today.getMonth() - e.target.value;
+  calcMonth.innerHTML = month;
+});
+
+birthYear.addEventListener("input", function (e) {
+  if (e.target.value < 1900 || e.target.value > 2024) {
+    yError.style.display = "block";
+  } else {
+    yError.style.display = "none";
+  }
+  age = today.getFullYear() - e.target.value;
+  calcYear.innerHTML = age;
 });
