48e87aa94781a2dafa95381325108d801accdafb
ImranGuluzada <imranguluzadeh@gmail.com>
2023-06-07 21:50:05 +0400

    rules

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-const birthDay = document.querySelector("#day");
-const birthMonth = document.querySelector("#month");
-const birthYear = document.querySelector("#year");
+const birthDay = document.querySelector(".day");
+const birthMonth = document.querySelector(".month");
+const birthYear = document.querySelector(".year");
 const inputs = document.querySelectorAll("input");
+const dError = document.querySelector("#dayError");
 
 const submit = document.getElementById("submit");
 
+birthDay.addEventListener("input", function (e) {
 
-
-function inputNumber(num) {
-  var ASCIICode = num.which ? num.which : num.keyCode;
-  if (ASCIICode > 31 && ASCIICode > 57) {
-    alert("Enter numbers only");
-  }
-}
-
-submit.addEventListener("click", validate);
-
-function validate(e) {
-  e.preventDefault();
-
-  let valid = true;
-
-  if (!typeof e === "number") {
-    return;
-  }
-  return valid;
-}
+    console.log(e.target.value);
+});
